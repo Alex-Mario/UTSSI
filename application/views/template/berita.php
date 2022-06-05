0b438dc9647da66fbb5fdc1cad3b472f9e76df41
archieved16@gmail.com <alex.mario1404@gmail.com>
2022-06-05 19:55:04 +0700

    first commit

---
+++ application/views/template/berita.php
@@ -0,0 +1,27 @@
+<section class="container">
+    <div class="row mb-3">
+        <div class="col-sm-6">
+            <h1>Contoh Judul Berita</h1>
+        </div>
+        <div class="col-sm-6 d-flex align-items-center justify-content-end" style="gap:10px;">
+            <small>Contoh Nama Penulis | 05/06/2022</small>
+            <img src=" https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" style="width:48px; height:48px;" class="rounded-circle border">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <img src="https://images.unsplash.com/photo-1498598457418-36ef20772bb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" class="w-100 mb-3" style="height: 70vh;">
+        </div>
+    </div>
+    <div class="row container">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et odio sollicitudin, pharetra libero eu, volutpat lorem. Integer ac nisi eget ex pretium dignissim ac ac sapien. Phasellus id dolor et orci lobortis tristique. Donec sapien justo, viverra vitae bibendum eget, vestibulum vitae sem. Duis cursus gravida fermentum. In at turpis luctus dolor eleifend posuere ac vitae felis. Aliquam efficitur hendrerit dui, vel pulvinar ante.
+
+        In suscipit eros sed turpis blandit euismod. Vestibulum eget diam venenatis, luctus nisi et, rutrum eros. Donec pellentesque nec libero eget aliquam. Duis commodo nulla ut placerat feugiat. Aliquam eget ex sed nulla eleifend fringilla vel in sem. Maecenas elementum libero porttitor nibh ultrices, vel viverra ex semper. Fusce consequat erat venenatis purus fringilla, sit amet dictum lorem semper. Suspendisse at nisi convallis, aliquet quam eu, condimentum urna. Sed tempor lectus id urna feugiat, et ornare eros interdum. Nulla quis dui sed erat commodo aliquet. Maecenas pretium vestibulum magna. Pellentesque tincidunt auctor arcu, in mattis dolor fringilla eu. Quisque consequat ipsum purus, at tincidunt turpis pharetra vel. Suspendisse vestibulum varius magna a sollicitudin.
+
+        Sed lectus enim, ornare at est et, vehicula mollis mi. In quis purus dolor. Nam non egestas nisl. Nunc ligula lectus, eleifend quis elementum ac, dictum feugiat urna. In ut odio et purus rutrum fringilla. Fusce et arcu quis elit hendrerit semper nec quis risus. Integer velit enim, elementum non neque a, facilisis vulputate purus. Pellentesque a lorem sit amet leo sagittis venenatis. Quisque tincidunt elit quis libero sagittis faucibus. Phasellus quis velit eget ante laoreet feugiat. Sed pretium pellentesque porttitor. Praesent vestibulum dignissim erat, a luctus est ultrices at. Quisque quis nisi a urna facilisis dignissim quis ac diam. Fusce lobortis purus non porttitor suscipit.
+
+        Aenean a elit ac ante tincidunt ultrices. Quisque vitae consectetur ligula. Ut accumsan risus at nunc convallis interdum. Fusce laoreet ut lectus semper faucibus. Integer ornare libero nibh, sed maximus ligula varius pharetra. Aliquam ultrices, nisl in vehicula sodales, erat lectus tincidunt odio, quis pulvinar risus mi ut dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec eleifend suscipit velit, at tempus erat laoreet sit amet. Phasellus ut augue ultrices, dignissim eros vel, ornare lacus. Morbi vitae sapien lobortis, accumsan libero nec, molestie quam. Etiam lorem elit, interdum egestas iaculis bibendum, aliquet nec nulla.
+
+        Suspendisse potenti. Morbi placerat feugiat sem sit amet ultricies. Aliquam erat volutpat. Duis quis velit eu orci aliquam posuere sit amet sit amet tortor. Sed dapibus ultrices augue, eget ultrices odio fringilla a. Nullam ut nisl magna. Cras quis risus leo.
+    </div>
+</section>
