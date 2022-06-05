f41e261d9330059866bb334c9f2078f6de9186b4
Brian Maxwell Ketaren <86067355+brianketaren14@users.noreply.github.com>
2022-06-05 20:38:08 +0700

    Update header.php

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>UTS SI2</title>
+	<link rel="icon" href="https://cdn-icons-png.flaticon.com/512/3313/3313480.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <meta name="theme-color" content="#712cf9">
 </head>
