b4683b39116920056195fd239bb274988501f120
Brian Maxwell Ketaren <86067355+brianketaren14@users.noreply.github.com>
2022-06-05 20:14:33 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/UTSSI';
+$config['base_url'] = 'http://localhost/UTS-SI2';
 
 /*
 |--------------------------------------------------------------------------
